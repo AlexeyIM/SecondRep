55c799ad76257640e16eae9b81d743860288f842
alfim <alfim@LAPTOP-8DQ4BML5>
2024-02-13 17:37:26 +0300

    Добавьте файлы проекта.

---
+++ project1/project1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+int main()
+{   
+    setlocale(0, "");
+    int a, b; 
+    std::cout << "Введите 2 числа";
+    std::cin >> a >> b;
+    std::cout << "Ответ:" << a + b;
+}
+
