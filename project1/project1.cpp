f7df7a68001adec9040fe8fd35f4f44eb099f9ad
Алексей da <alfimovalex14@gmail.com>
2024-02-16 14:55:36 +0300

    я правда не хочу запутаться вот я написал комментарий я крут! это сделал леха.

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -3,13 +3,12 @@
 using namespace std;
 
 int main()
-{   
+{
     setlocale(0, "");
-    int a, b; 
+    int a, b;
     cout << "Введите 2 числа\n";
     cin >> a >> b;
     cout << "Сумма: " << a + b << endl;
     cout << "Произведение: " << a * b;
     cout << "РАЗНОСТЬ: " << a - b;
 }
-
